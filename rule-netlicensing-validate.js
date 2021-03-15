7445341b8559486b77fe2f47b3e420366a14fc82
Rudkovskiy <xraskolx@gmail.com>
2021-03-15 23:50:14 +0200

    Improve js file formatting (#2)
    * improve js file formatting
    
    * move imports in the function + add comments
    
    Co-authored-by: Alexey Averikhin <alexey.averikhin@labs64.de>

--- rule-netlicensing-validate.js
+++ rule-netlicensing-validate.js
@@ -1,26 +1,41 @@
 function validateNetLicensing(user, context, callback) {
-  if (!configuration.NETLICENSING_API_KEY) {
-    console.log('Missing required configuration: NetLicensing API_KEY; skipping.');
-    return callback(null, user, context);
-  }
-
-  const { NETLICENSING_BASE_URL = 'https://go.netlicensing.io/core/v2/rest' } = configuration;
-  const { NETLICENSING_API_KEY } = configuration;
-  const { NETLICENSING_PRODUCT_NUMBER } = configuration;
-  const { NETLICENSING_PRODUCT_MODULE_NUMBER } = configuration;
 
   const qs = require('qs');
   const request = require('request');
 
-  // skip if no user email available (used as NetLicensing customer number)
-  if (!user.email) {
+  const {
+    NETLICENSING_API_KEY: apiKey,
+    NETLICENSING_BASE_URL: baseUrl = 'https://go.netlicensing.io/core/v2/rest',
+    NETLICENSING_PRODUCT_NUMBER: productNumber,
+    NETLICENSING_PRODUCT_MODULE_NUMBER: productModuleNumber,
+  } = configuration;
+
+  // verify mandatory configuration
+  if (!apiKey) {
+    console.error('Missing required configuration: NetLicensing API_KEY; skipping.');
     return callback(null, user, context);
   }
 
-  var isExpired = false;
-  if (user.user_metadata && user.user_metadata.netlicensing) {
-    console.log('User metadata found; TTL=' + new Date(user.user_metadata.netlicensing.ttl));
-    if ((new Date(user.user_metadata.netlicensing.ttl)).getTime() < (new Date()).getTime()) {
+  const { user_id, email, name, user_metadata = {} } = user;
+
+  const licenseeNumber = email;
+  const licenseeName = name;
+
+  // skip if no customer number (user email) set
+  if (!licenseeNumber) {
+    return callback(null, user, context);
+  }
+
+  // verify not existing or expired validation response
+  let isExpired = false;
+  if (user_metadata.netlicensing) {
+    const { ttl } = user_metadata.netlicensing;
+    const ttlDate = new Date(ttl);
+    const now = new Date();
+
+    console.log(`User metadata found; TTL=${ttlDate}`);
+
+    if (ttlDate.getTime() < now.getTime()) {
       isExpired = true;
       console.log('User metadata expired');
     }
@@ -29,71 +44,60 @@ function validateNetLicensing(user, context, callback) {
     console.log('No user metadata found');
   }
 
-  // Skip NetLicensing validation if exists and not expired yet
+  // request NetLicensing validation
   if (isExpired) {
-
-    console.log('Send NetLicensing validation request for ' + user.email);
-
-    let auth = 'Basic ' + Buffer.from('apiKey:' + NETLICENSING_API_KEY).toString('base64');
-    let data = {
-      licenseeName: user.name
-    };
-    data.productNumber = NETLICENSING_PRODUCT_NUMBER;
-    data.productModuleNumber = NETLICENSING_PRODUCT_MODULE_NUMBER;
+    console.log(`Send NetLicensing validation request for ${licenseeNumber}`);
 
     request.post(
-      `${NETLICENSING_BASE_URL}/licensee/${user.email}/validate`,
+      `${baseUrl}/licensee/${licenseeNumber}/validate`,
       {
         headers: {
-          Authorization: auth,
+          Authorization: `Basic ${Buffer.from('apiKey:' + apiKey).toString('base64')}`,
           Accept: 'application/json',
           'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: qs.stringify(data)
+        body: qs.stringify({ licenseeName, productNumber, productModuleNumber })
       },
       (httpError, response, body) => {
         if (httpError) {
-          console.error('Error calling NetLicensing API: ' + httpError.message);
+          console.error(`Error calling NetLicensing API: ${httpError.message}`);
           // swallow NetLicensing api errors and just continue login
           return callback(null, user, context);
         }
 
-        // if we reach here, it means NetLicensing returned info and we'll add it to the metadata
-
-        let parsedBody;
+        // if we reach here, it means NetLicensing returned validation response and we'll add it to the metadata
+        let parsedBody = '';
 
         try {
           parsedBody = JSON.parse(body);
         } catch (parseError) {
-          console.error(
-            'Error parsing NetLicensing response: ' + parseError.message
-          );
+          console.error(`Error parsing NetLicensing response: ${parseError.message}`);
           return callback(null, user, context);
         }
 
-        user.user_metadata = user.user_metadata || {};
-        user.user_metadata.netlicensing = parsedBody;
-
+        // update user metadata with NetLicensing response
         try {
-          auth0.users.updateUserMetadata(user.user_id, user.user_metadata);
+          auth0.users.updateUserMetadata(user_id, { ...user_metadata, netlicensing: parsedBody });
         } catch (auth0Error) {
-          console.error('Error updating the user profile: ' + auth0Error.message);
+          console.error(`Error updating the user profile: ${auth0Error.message}`);
           return callback(null, user, context);
         }
 
-        // Add user metadata to the /userinfo context
-        const netlicensingData = JSON.parse(JSON.stringify(parsedBody));
-        delete netlicensingData.details;
-        context.idToken['https://netlicensing.io/auth0'] = netlicensingData;
+        // add user metadata to the /userinfo context
+        const data = JSON.parse(JSON.stringify(parsedBody));
+        delete data.details;
+
+        context.idToken['https://netlicensing.io/auth0'] = data;
 
         return callback(null, user, context);
       }
     );
   } else {
-    // Add user metadata to the /userinfo context
-    const netlicensingData = JSON.parse(JSON.stringify(user.user_metadata.netlicensing));
-    delete netlicensingData.details;
-    context.idToken['https://netlicensing.io/auth0'] = netlicensingData;
+    // add user metadata to the /userinfo context
+    const data = JSON.parse(JSON.stringify(user_metadata.netlicensing));
+    delete data.details;
+
+    context.idToken['https://netlicensing.io/auth0'] = data;
 
     return callback(null, user, context);
   }
